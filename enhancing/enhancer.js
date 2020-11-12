ad7e05fb526203879019f083db0e22eb555128ed
bhawnishk1 <mrbhawnish@gmail.com>
2020-11-11 17:03:26 -0800

    MVP completed, with functions and tests working

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -6,15 +6,39 @@ module.exports = {
 };
 
 function success(item) {
-  return { ...item };
+  // The item's enhancement increases by 1.
+  // If the item enhancement level is 20, the enhancement level is not changed.
+  // The durability of the item is not changed.
+  let newEnhancement = 0;
+  if(item.enhancement < 20) {
+     newEnhancement = item.enhancement + 1;
+
+  }
+
+  return { ...item, enhancement: newEnhancement  }
 }
 
+
 function fail(item) {
+//  If the item's enhancement is less than 15, the durability of the item is decreased by 5.
+// If the item's enhancement is 15 or more, the durability of the item is decreased by 10.
+// If the item's enhancement level is greater than 16, the enhancement level decreases by 1 (17 goes down to 16, 18 goes down to 17).
+
+
+if(item.enhancement < 15){
+  item.durability -= 5
+} else if(item.enhancement >= 15){
+  item.durability -= 10
+} 
+
+if(item.enhancement > 16){
+   --item.enhancement
+}
   return { ...item };
 }
 
 function repair(item) {
-  return { ...item };
+  return { ...item, durability: 100 };
 }
 
 function get(item) {
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,42 @@
-const enhancer = require('./enhancer.js');
+const { success, fail, repair} = require('./enhancer.js');
 // test away!
+
+describe("test functionality for the game's enhancing system", () => {
+ 
+    describe('repair function that returns 100 output regardless of the argument', () => {
+    it('repair function accepts an object and returns with 100 of durability', () => {
+        expect(repair({name: "item", durability: 300000, enhancement: 20}))
+        .toMatchObject({name: "item", durability: 100, enhancement: 20})
+    })
+})
+    describe('success function accepts an object and returns a modified object with following:', () => {
+        it('success function enhancement increases by 1', () => {
+            expect(success({name: "item", durability: 100, enhancement: 4}))
+            .toMatchObject({name: "item", durability: 100, enhancement: 5})
+       }) 
+    })
+    describe('fail function accepts an object and returns a modified object with following:', () => {
+        it("if the item's enhancement is less than 15, the durability decreases by 5", () => {
+            expect(fail({name: "item", durability: 100, enhancement: 14}))
+            .toMatchObject({name: "item", durability: 95, enhancement: 14})
+           
+       }) 
+
+        it("if the item's enhancement is more than or equals 15, the durability decreases by 10", () => {
+        expect(fail({name: "item", durability: 100, enhancement: 15}))
+        .toMatchObject({name: "item", durability: 90, enhancement: 15})
+
+        expect(fail({name: "item", durability: 100, enhancement: 16}))
+        .toMatchObject({name: "item", durability: 90, enhancement: 16})
+       
+        })
+
+        it("if the item's enhancement is more than 16, the durability decreases by 10 and enhancement decreases by 1", () => {
+            expect(fail({name: "item", durability: 100, enhancement: 19}))
+            .toMatchObject({name: "item", durability: 90, enhancement: 18})
+           
+            })
+       
+    })
+   
+})
